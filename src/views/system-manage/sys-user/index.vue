ad8e11b3fb749e3973af772165196093661397c6
xufang775 <xufang461010923@163.com>
2018-11-30 23:35:22 +0800

    添加用户管理的相关视图

---
+++ src/views/system-manage/sys-user/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="app-container">
+      <div class="filter-container">
+        <el-button class="filter-item" type="primary" icon="el-icon-add" size="mini" @click="handleAdd">新增</el-button>
+      </div>
+      <div class=""></div>
+      {{addEditVisible}}
+      <d-add-edit :pVisible="addEditVisible" :pStatus="'create'" @visibleChange="hideAddEdit"></d-add-edit>
+    </div>
+</template>
+
+<script>
+    import DAddEdit from "./d-add-edit";
+
+    export default {
+      components: {DAddEdit},
+      name: "index",
+      data(){
+        return {
+          addEditVisible:false
+        }
+      },
+      methods:{
+        handleAdd(){
+          this.addEditVisible = true;
+        },
+        hideAddEdit(data){
+          this.addEditVisible = data;
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
