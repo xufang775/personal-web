862b7ecd9662f45f597219e1fbf43be73a94ff4d
xufang775 <xufang461010923@163.com>
2018-11-30 23:37:15 +0800

    重命名BaseDialog为base-dialog,并修改其内容

---
+++ src/components/Base/base-dialog.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "base-dialog",
+    props:['pVisible','pStatus'],
+    data(){
+        return {
+          title:'',
+          visible:this.pVisible,
+          status:this.pStatus,
+          textMap: {
+            update: '修改',
+            create: '创建'
+          },
+          model:{},
+          rules:{},
+        }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
